b37b9a2de3077d6da7bd35727a5fec617efeeb59
Christian Baumann <33223146+chbauman@users.noreply.github.com>
2021-04-07 09:52:33 +0200

    Changed Todo to a functional component

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,50 +1,44 @@
-import React, { Component, Button } from "react";
+import React, { Component, useState } from "react";
 
-class Todo extends Component {
-  state = {
-    value: this.props.todo_data.value,
-  };
-  styles = {
-    fontSize: "20px",
-    "border-style": "solid",
-    margin: "3px",
-    padding: "3px",
-    "border-radius": "5px",
-  };
 
-  render() {
-    return (
-      <React.Fragment>
-        <div className="row" style={this.styles} data-testid="todo">
-          <span className="btn-group col-md-9 col-lg-9 col-sm-9 col-xs-9">
-            {this.renderText()}
-          </span>
-          <div className="btn-group col-md-3 col-lg-3 col-sm-3 col-xs-3">
-            <button
-              className="btn-sm btn-secondary"
-              onClick={() => {
-                this.props.onDelete(this.props.todo_data.text);
-              }}
-            >
-              Done<span className="glyphicon glyphicon-ok"></span>
-            </button>
-            <button
-              className="btn-sm btn-danger"
-              onClick={() => {
-                this.props.onDelete(this.props.todo_data.id);
-              }}
-            >
-              Delete
-            </button>
-          </div>
+const styles = {
+  fontSize: "20px",
+  borderStyle: "solid",
+  margin: "3px",
+  padding: "3px",
+  borderRadius: "5px",
+};
+
+function Todo(props) {
+  const [value, setValue] = useState(props.todo_data.value);
+
+  return (
+    <React.Fragment>
+      <div className="row" style={styles} data-testid="todo">
+        <span className="btn-group col-md-9 col-lg-9 col-sm-9 col-xs-9">
+          {value}
+        </span>
+        <div className="btn-group col-md-3 col-lg-3 col-sm-3 col-xs-3">
+          <button
+            className="btn-sm btn-secondary"
+            onClick={() => {
+              props.onDelete(props.todo_data.text);
+            }}
+          >
+            Done<span className="glyphicon glyphicon-ok"></span>
+          </button>
+          <button
+            className="btn-sm btn-danger"
+            onClick={() => {
+              props.onDelete(props.todo_data.id);
+            }}
+          >
+            Delete
+          </button>
         </div>
-      </React.Fragment>
-    );
-  }
-
-  renderText() {
-    return this.state.value;
-  }
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Todo;
